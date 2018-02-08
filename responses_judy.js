8440aa2a2de64fbf65ca5b772a24234306fbc663
JudyPuff <judy.yun.ting@gmail.com>
2018-02-08 21:36:44 +1300

    finished judys words

--- responses_judy.js
+++ responses_judy.js
@@ -6,7 +6,40 @@ Who:{
   "That lump of foul deformity",
 },
 
-What:{
+What/Which:{
   "Blood of your first born",
-  "Brie"
+  "Your infected mind has no cure",
+  "I bid thee away, fair maiden!",
+  "Why would you even ask that?",
+  "How the hell did you come up with that?"
+},
+
+When: {
+  "Let me check with my secretary",
+  "That time you said you love me",
+  "When you dropped your ice cream",
+  "You'll know when you grow up",
+  "Tomorrow"
+},
+
+How: {
+  "With greatness",
+  "Love conquers all",
+  "Let me count the way",
+  "By mixing it all up",
+  "I'd hate to guess what's going through your mind"
+}
+Where: {
+  "At ninny's tomb",
+  "Somewhere over the rainbow",
+  "Way up high",
+  "Never Never Land",
+  "Right up my alley"
+}
+Why:{
+  "The tartness of his face sours ripe grapes",
+  "It just happens",
+  "They always do",
+  "Must be fate",
+  "You should be shot for asking that"
 }
