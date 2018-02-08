e7982aa89ba607e226373e6bb63e862a0e6b307d
timothy-tolley <timothytolley@outlook.com>
2018-02-08 20:28:28 +1300

    added tim's responses

---
+++ responses_tim.js
@@ -0,0 +1,43 @@
+Who:
+"Homer Simpson, doh!",
+"Frankenstein, naturally",
+"ever heard of Julius Caesar",
+"Adam, cause its his birthday",
+"Jacinda"
+
+What:
+"Bananas, everyone loves bananas.",
+"Sugar and spice and everything nice ",
+"Sorry, watching married at first sight",
+"A phlebotomist's worst nightmare",
+"Seven deadly sins, you dingbat"
+
+
+Where:
+"Turn around",
+"Have you checked your mum's closet?",
+"The dog box",
+"The blackhole of nothingness, naturally",
+"A tattoo parlor"
+
+Why:
+"Because you're worth it",
+"Ask your mum",
+"I blame the dog",
+"Don't worry your little mind",
+"Whale, whale, whale"
+
+How:
+"Poorly, obviously",
+"Go with the flow",
+"I dunno, Ask Don",
+"No clue, mate",
+"You're on your own"
+
+
+Will:
+"Couldn't say"
+"Not in a million years",
+"When pigs fly",
+"yes, but in a galaxy far away",
+"yes but not for you",
