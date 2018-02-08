7f06f7b13f018bda17ed61d0858e4de08e939467
Judy Ting-Edwards <judy.yun.ting@gmail.com>
2018-02-08 18:11:41 +1300

    started database

---
+++ responses_judy.js
@@ -0,0 +1,12 @@
+Who:{
+  "Your mum",
+  "Someone far far away",
+  "Your worst nightmare!",
+  "That cow next door",
+  "That lump of foul deformity",
+},
+
+What:{
+  "Blood of your first born",
+  "Brie"
+}
