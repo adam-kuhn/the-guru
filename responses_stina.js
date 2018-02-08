61d2cbfb85e24590d30e2ec61528a3bbc924f376
Kristina Muller <Stina@Kristinas-MacBook-Pro-2.local>
2018-02-08 20:02:09 +1300

    added responses and images

---
+++ responses_stina.js
@@ -0,0 +1,49 @@
+Who:
+'the answer to this is always, Beyonce'
+'Don.'
+'He who must not be named'
+'Olaf'
+'Ronald Weasly'
+
+What:
+'forty two'
+'not sure, sorry'
+'Cloudy, with a chance of Rain'
+'cats and dogs'
+'french fries'
+
+Where:
+'The beach'
+'Dev Academy'
+'Follow the yellow brick road'
+'At the end of the rainbow'
+'Dons house'
+
+Why:
+'Because thats life'
+'There is no answer to this'
+'Because Beyonce, thats why'
+'You shall never know'
+'This response is to deep for you'
+
+How:
+'Rinse and Repeat'
+'Use Boiling Water'
+'Alovera cures all'
+'If its still itchy, seek medical attention'
+'Ginger Tea is great for this'
+
+Will:
+'Not in this lifetime'
+'The answer is within you'
+'Yaaaaass!'
+'Nope'
+'Well... maybe'
+
+When:
+'At 5pm'
+'When the moon is in the 7th house'
+'Never. Sorry'
+'When you realise your inner truth'
+'In a few years, possibly'
+
