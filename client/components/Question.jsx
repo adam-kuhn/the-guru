216a81493d257192b3fb989fa2510adebce8b4b2
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-09 11:17:22 +1300

    handle state on question, working sending to server

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import request from 'superagent'
 
 class Question extends React.Component{
   constructor(props) {
@@ -7,22 +8,35 @@ class Question extends React.Component{
       question: ''
     }
     this.getQuestion = this.getQuestion.bind(this)
+    this.sendQuestion = this.sendQuestion.bind(this)
   }
   getQuestion(evt){
     this.setState({
       [evt.target.name]: evt.target.value
     })
   }
+
+  sendQuestion() {
+    request
+      .post('/api/v1/responses')
+      // .set('Content-Type', 'application/json')
+      // .set('Accept', 'text/plain')
+      .send(this.state)
+      // console.log('sent', this.state)
+      // .then (() => {
+      //   res.send(this.state)
+      //           // console.log('complete')
+      // })
+  }
   render() {
     return (
       <div>
         <form>
           <label>What would you like to know?
-             {/*add onChange  */}
             <input type='text' name='question' onChange={this.getQuestion} />
           </label>
           {/* add onClick */}
-          <button type='button'>Submit Your Question</button>
+          <button type='button' onClick={this.sendQuestion}>Submit Your Question</button>
         </form>
       </div>
     )
